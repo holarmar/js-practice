67a881fd3704b82d771694b176cab808e3c36cb1
rcdmar <hola@rcdmar.com>
2019-04-09 22:41:34 -0500

    Add js

--- 4_simple_calculator/main.js
+++ 4_simple_calculator/main.js
@@ -1,3 +1,20 @@
 function calculateResult(){
+  var firstVal = parseFloat(document.getElementById('firstValue').value);
+      secondVal = parseFloat(document.getElementById('secondValue').value);
+      operator =  document.getElementById('operatorChoice').value;
+      result = ""
+
+  if (operator == "add") {
+    result = firstVal + secondVal;
+  } else if (operator == "min") {
+    result = firstVal - secondVal;
+  } else if (operator == "div") {
+    result = firstVal / secondVal;
+  } else if (operator == "mul") {
+    result = firstVal * secondVal;
+  }
+
+
+  document.getElementById('resultId').innerHTML = result
 
 };
