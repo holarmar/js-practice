50e5ada7af31833ae60901c1e75a6f6f701f6688
rcdmar <hola@rcdmar.com>
2019-04-09 22:40:29 -0500

    Fix '-x' days but 366 shows instead of 0 when date is the same

--- 3_days_left_for_bday/main.js
+++ 3_days_left_for_bday/main.js
@@ -1,26 +1,28 @@
-function calculateDays(){
-var today = new Date();
-var bday = new Date(today.getFullYear(), document.getElementById('bdayMonth').value - 1, document.getElementById('bdayDay').value)
-if (today.getTime() > bday.getTime()) {
-  bday.setFullYear(bday.getFullYear() + 1);
-}
-var one_day = 1000 * 60 * 60 * 24;
-var isBirthday = ""
-var diff = Math.abs(bday.getTime() - today.getTime());
-var result = Math.ceil((diff) / (one_day));
+function calculateDays() {
+  var today = new Date();
+  var bday = new Date(today.getFullYear(), document.getElementById('bdayMonth').value - 1, document.getElementById('bdayDay').value)
+  if (today.getTime() > bday.getTime()) {
+    bday.setFullYear(bday.getFullYear() + 1);
+  }
+  var one_day = 1000 * 60 * 60 * 24;
+  var isBirthday = ""
+  var diff = Math.abs(bday.getTime() - today.getTime());
+  var result = Math.ceil((diff) / (one_day));
 
-if(result == "0") {
-  isBirthday = " días para tu cumpleaños. ¡Felicidades!";
-} else {
+  if (result == "0") {
+    isBirthday = " días para tu cumpleaños. ¡Felicidades!";
+  } else {
     isBirthday = " días para tu cumpleaños!";
-};
+  };
 
-document.getElementById('resultArea').innerHTML = result + isBirthday;
+  console.log()
 
-console.log(diff);
-console.log(result);
-console.log(today);
-console.log(bday);
+  document.getElementById('resultArea').innerHTML = result + isBirthday;
+
+  console.log(diff);
+  console.log(result);
+  console.log(today);
+  console.log(bday);
 
 
 };
