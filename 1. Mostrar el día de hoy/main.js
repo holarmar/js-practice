a86c569c865e89f5da2a5695e8d3e187fca1341d
rcdmar <hola@rcdmar.com>
2019-04-09 22:17:23 -0500

    New projects

--- 1. Mostrar el día de hoy/main.js
+++
@@ -1,5 +0,0 @@
-var today = new Date();
-var day = today.getDay();
-var daylist = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-
-document.getElementById("currentDate-id").innerHTML = daylist[day];
