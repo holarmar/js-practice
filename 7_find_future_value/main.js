e6f279c440142ddae68179df848d4b33f7c962b2
rcdmar <hola@rcdmar.com>
2019-04-12 15:49:58 -0500

    Update

--- 7_find_future_value/main.js
+++ 7_find_future_value/main.js
@@ -1,3 +1,25 @@
-function getFV(){
-  
+function getValue(){
+  var type = document.getElementById('selectType').value;
+      comp = document.getElementById('isCompound').checked;
+      cont = document.getElementById('isContiuous').checked;
+      quant = parseInt(document.getElementById('idQuantity').value);
+      time = parseInt(document.getElementById('idTime').value);
+      intR = (parseFloat(document.getElementById('idRate').value) / 100);
+      compP = parseInt(document.getElementById('idCompounding').value);
+      effR = "";
+
+  if (comp == true && cont == false){
+    effR = Math.pow((intR / compP), compP);
+  } else {
+    effR = 5;
+  }
+
+  console.log(type);
+  console.log(comp);
+  console.log(cont);
+  console.log(quant);
+  console.log(time);
+  console.log(intR);
+  console.log(compP);
+  console.log(effR);
 }
